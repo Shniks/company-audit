af54c3bd836f16748a202760da4e879f4a1dd284
Lihkin <shniks@gmail.com>
2018-02-27 10:51:31 -0700

    Completing load_employees method

--- lib/company.rb
+++ lib/company.rb
@@ -14,17 +14,16 @@ class Company
 
   def load_employees(filename)
     CSV.foreach(filename, headers: false) do |data|
-      return(data, )
-      @employees << data
+      @status = returns(data, 5)
+      @employees << data if @status[:success] == true && @status[:error].nil?
     end
+    @status
   end
 
-  def return(data, size)
+  def returns(data, size)
     return_value = { success: true, error: nil }
-    data.each do |data|
-      return_value[:error] = 'bad data' if data.length < size
-    end
-    return_value[:success] = true if return_value[:error] == nil
+    return_value[:error] = 'bad data' if data.length < size
+    return_value[:success] = true if return_value[:error].nil?
     return_value
   end
 
