26f45e613967de5b979e3e3f62da74a7dbc32ed5
Lihkin <shniks@gmail.com>
2018-02-27 09:30:34 -0700

    Completing testing for employee attributes.

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,17 +1,17 @@
 class Employee
 
-  attr_reader :employee_id,
+  attr_reader :id,
               :name,
               :role,
               :start_date,
               :end_date
 
   def initialize(employee_id, name, role, start_date, end_date)
-    @employee_id  = employee_id.to_i
+    @id           = employee_id.to_i
     @name         = name
     @role         = role
-    @start_date   = start_date
-    @end_date     = end_date
+    @start_date   = DateHandler.string_to_date(start_date)
+    @end_date     = DateHandler.string_to_date(end_date)
   end
 
 end
